06e1505cf2cddbfeafb7bbe5ceee3c8e0e8a3632
carduswork <1360998812@qq.com>
2016-03-22 21:31:06 +0800

    sortok

--- sirt/Program.cs
+++ sirt/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Collections;
 namespace sirt
 {
     class Program
     {
+        public static List<Employee> es = new List<Employee>();
         static void Main(string[] args)
         {
+            es.Add(new Employee("1", "1", "1"));
+            es.Add(new Employee("1", "1", "5"));
+            es.Add(new Employee("1", "1", "3"));
+            es.Sort(new SCompare());
+            foreach (Employee e in es)
+            { 
+            Console.WriteLine(e.Salary);
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ sirt/SCompare.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sirt
+{
+    class SCompare:IComparer<Employee>
+    {
+       
+
+        public int Compare(Employee x, Employee y)
+        {
+            //throw new NotImplementedException();
+            return (x.Salary.CompareTo(y.Salary));
+        }
+    }
+}
